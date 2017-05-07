71b35b088a57651c912a5d0094354cf783b3ce56
mbiel <matbiel@gmail.com>
2017-05-07 12:06:25 +0000

    add GET for users

--- src/Main.hs
+++ src/Main.hs
@@ -6,24 +6,30 @@ import Snap.Core
 import Snap.Util.FileServe
 import Snap.Http.Server
 import User
-import Database.MongoDB
+import qualified Database.MongoDB as DB
 import Control.Monad.Trans (liftIO)
 import Control.Monad.IO.Class
 import Data.ByteString
 import Data.Aeson
+import Data.Text.Encoding
+import Data.Maybe
+import Data.UUID
+import System.Random
+import Data.List
 
-getDatabase :: Database
+getDatabase :: DB.Database
 getDatabase = "my_database"
 
-getAccessMode :: AccessMode
-getAccessMode = master
+getAccessMode :: DB.AccessMode
+getAccessMode = DB.master
 
 getDatabaseAddr = "0.0.0.0"
 
 performAction action = do
-    pipe <- connect $ host $ getDatabaseAddr
-    e <- access pipe getAccessMode getDatabase action
-    close pipe
+    pipe <- DB.connect $ DB.host $ getDatabaseAddr
+    result <- DB.access pipe getAccessMode getDatabase action
+    DB.close pipe
+    return result
 
 main :: IO ()
 main = quickHttpServe site
@@ -32,7 +38,9 @@ site :: Snap ()
 site =
     ifTop (writeBS "hello world") <|>
     route [
-            ("users", method POST addNewUser)
+            ("users", method POST addNewUser),
+            ("users", method GET getUsers),
+            ("users/:userId", method GET getUser)
           ] <|>
     dir "static" (serveDirectory ".")
 
@@ -40,8 +48,30 @@ addNewUser :: Snap ()
 addNewUser = do
     body <- readRequestBody 2048
     user <- return (decode body)
-    maybe (writeBS "unable to parse JSON")
-        (liftIO . performAction . insert "users" . userToDocument) user
+    maybe (writeBS "unable to parse JSON") insertUser user
 
+insertUser user = do
+    uuid <- liftIO $ randomIO
+    liftIO $ performAction $ DB.insert "users" $ userToDocument $ user {User.id = Just $ Data.UUID.toString uuid}
+    return ()
 
+getUser :: Snap ()
+getUser = do
+    param <- getParam "userId"
+    loadUser param
+
+loadUser :: Maybe ByteString -> Snap ()
+loadUser (Just param) = do
+    document <- liftIO $ performAction $ (DB.find (DB.select ["_id" DB.=: DB.UUID param] "users") >>= DB.rest)
+    returnDocument document
+loadUser Nothing = writeBS "userId is not specified"
+
+returnDocument :: [DB.Document] -> Snap ()
+returnDocument (a:ax) = writeLBS $ encode $ userFromDocument a
+returnDocument [] = writeBS "user does not exist"
+
+getUsers :: Snap ()
+getUsers = do
+    documents <- liftIO $ performAction $ (DB.find (DB.select [] "users") >>= DB.rest)
+    writeLBS $ encode $ liftToJSON toJSON toJSON $ Data.List.map userFromDocument documents
 
--- src/User.hs
+++ src/User.hs
@@ -2,16 +2,28 @@
 module User where
 
 import Database.MongoDB
-import Data.Text
+import qualified Data.Text as TXT
 import GHC.Generics
 import Data.Aeson
+import Data.UUID
+import Data.Maybe
+import qualified Data.ByteString.Char8 as BS
 
 data User = User {
+                    id :: Maybe String,
                     name :: String,
                     email :: String
                 } deriving (Show, Generic)
 
 instance FromJSON User where
+instance ToJSON User where
 
 userToDocument :: User -> Document
-userToDocument user = [pack "name" =: name user, pack "email" =: email user]
+userToDocument user = [TXT.pack "_id" =: new_id, TXT.pack "name" =: name user, TXT.pack "email" =: email user]
+    where new_id = fromMaybe Database.MongoDB.Null (fmap (val . Database.MongoDB.UUID . BS.pack) $ User.id user)
+
+userFromDocument :: Document -> User
+userFromDocument doc = User id name email
+    where id = Just $ (show :: Database.MongoDB.UUID -> String) $ typed $ valueAt (TXT.pack "_id") doc
+          name = typed $ valueAt (TXT.pack "name") doc
+          email = typed $ valueAt (TXT.pack "email") doc

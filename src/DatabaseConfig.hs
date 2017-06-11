7fa7161ea171f1a4162daaa0f332cf33254d161c
mbiel <matbiel@gmail.com>
2017-06-11 15:17:44 +0000

    read database conf from json file

--- src/DatabaseConfig.hs
+++ src/DatabaseConfig.hs
@@ -1,11 +1,18 @@
+{-# LANGUAGE DeriveGeneric #-}
 module DatabaseConfig where
 
 import qualified Database.MongoDB as DB
 
+import Data.Aeson
+import GHC.Generics
+
 data DatabaseConfig = DatabaseConfig {
-    databse_addr :: String,
+    database_addr :: String,
     database_name :: String
-}
+} deriving (Show, Generic)
+
+instance FromJSON DatabaseConfig where
+instance ToJSON DatabaseConfig where
 
 connectDatabase :: DatabaseConfig -> IO DB.Pipe
-connectDatabase config = DB.connect $ DB.host $ databse_addr config
+connectDatabase config = DB.connect $ DB.host $ database_addr config
--- src/Main.hs
+++ src/Main.hs
@@ -6,27 +6,52 @@ import Hetzer
 
 import qualified UsersRepository as UR
 import qualified Database.MongoDB as DB
+import qualified Data.List as List
+import qualified System.IO as IO
+import qualified Data.ByteString.Lazy.Char8 as BSC8
+import qualified Data.ByteString.Lazy as LBS
 
 import Control.Applicative
 import Snap.Core
 import Snap.Util.FileServe
 import Snap.Http.Server
 import Snap.Snaplet
-import Data.ByteString
 import Data.Text.Encoding
 import Control.Applicative
 import Control.Monad
 import Control.Monad.State
-
+import System.Environment
+import Data.Maybe
+import Data.Either.Unwrap
+import Data.Aeson
 
 hetzerInit db_conf pipe = makeSnaplet "hetzer" "hetzer" Nothing $ do
     addRoutes UR.handlers
     return $ Hetzer db_conf pipe
 
+getConfFilePath :: [String] -> Maybe FilePath
+getConfFilePath [] = Just "./hetzer_conf.json"
+getConfFilePath (a:ax) | a == "--config" = getPath $ List.take 1 ax
+                       | otherwise = getConfFilePath ax
+
+getPath :: [String] -> Maybe FilePath
+getPath [] = Nothing
+getPath (a:ax) = Just a
+
 main :: IO ()
 main = do
-    db_conf <- return $ DatabaseConfig "0.0.0.0" "my_database"
-    pipe <- connectDatabase db_conf
-    serveSnaplet defaultConfig (hetzerInit db_conf pipe)
-    DB.close pipe
+    args <- getArgs
+    path <- return $ getConfFilePath args
+    if (isJust path) then do
+        content <- IO.readFile (fromJust path)
+        decoded <- return $ (eitherDecode :: LBS.ByteString -> Either String DatabaseConfig) $ BSC8.pack content
+        if (isRight decoded) then do
+            db_conf <- return $ fromRight decoded
+            pipe <- connectDatabase db_conf
+            serveSnaplet defaultConfig (hetzerInit db_conf pipe)
+            DB.close pipe
+        else
+            IO.putStrLn $ fromLeft decoded
+    else
+        IO.putStrLn "Config file path is not specified."
 

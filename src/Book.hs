f9b9cc993ae9d8af4026c178139b974f6ee5fe1b
mbiel <matbiel@gmail.com>
2017-06-18 10:51:14 +0000

    add Book datatype

---
+++ src/Book.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE DeriveGeneric #-}
+module Book where
+
+import Date
+
+import GHC.Generics
+import Data.Aeson
+import Data.Maybe
+import Data.UUID
+
+data Book = Book {
+                    id :: Maybe UUID,
+                    title :: String,
+                    author :: String,
+                    isbn :: String,
+                    genere :: String,
+                    published :: Date,
+                    quantity :: Maybe Int,
+                    left_in_stock :: Maybe Int
+                } deriving (Show, Generic)
+
+instance FromJSON Book where
+instance ToJSON Book where
+

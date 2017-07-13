7d23a17195b3d205b1ed52d32c4a2ae165600703
mbiel <matbiel@gmail.com>
2017-07-13 21:12:15 +0000

    chnage div id. add ./dist to gitignore

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import axios from 'axios';
 
 import { Hello } from "./components/Hello";
 
 ReactDOM.render(
     <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("example")
+    document.getElementById("root")
 );
+
